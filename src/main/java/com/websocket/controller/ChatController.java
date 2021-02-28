0f47f2609fff8b31b7dc662a16cb57b753d9fb1a
변성재 <dfdo333@naver.com>
2021-02-28 23:02:01 +0900

    redis version ftl => ftlh

--- src/main/java/com/websocket/controller/ChatController.java
+++ src/main/java/com/websocket/controller/ChatController.java
@@ -2,28 +2,34 @@ package com.websocket.controller;
 
 import java.util.List;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.websocket.dao.ChatRoomRepository;
+import com.websocket.dto.ChatMessage;
 import com.websocket.dto.ChatRoom;
-import com.websocket.service.ChatService;
+import com.websocket.pubsub.RedisPublisher;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class ChatController {
-	private final ChatService chatService;
+	private final RedisPublisher redisPublisher;
+	private final ChatRoomRepository chatRoomRepository;
 
-	@PostMapping("/chat")
-	public ChatRoom createRoom(@RequestParam String name) {
-		return chatService.createRoom(name);
-	}
-
-	@GetMapping("/chat")
-	public List<ChatRoom> findAllRoom() {
-		return chatService.findAllRoom();
+	@MessageMapping("/chat/message")
+	public void message(ChatMessage message) {
+		if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+			chatRoomRepository.enterChatRoom(message.getRoomId());
+			message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+		}
+		
+		redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
 	}
 }
